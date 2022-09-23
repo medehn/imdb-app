4d33acc1ddde7db681432ca34d630bf6eb35b7c3
Mel <medehn85@gmail.com>
2022-09-23 13:28:24 +0200

    IMDB api calls setup

--- src/app/actor/actor.service.ts
+++ src/app/actor/actor.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
 import { IActor } from './actor';
 import { ACTORDATA } from './imdbdata';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ActorService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
+  actor: any | undefined;
   getActorData(): IActor {
+    this.http
+      .get<any>('https://imdb-api.com/en/API/SearchName/k_bui3yj8o/Jean%20Reno')
+      .subscribe((data) => {
+        console.log(data);
+        this.actor = data;
+        ACTORDATA.name = "From API: "+ this.actor.expression;
+        ACTORDATA.id = "From API: "+this.actor.results[0].id;
+        console.log(ACTORDATA)
+      });
+
     return ACTORDATA;
   }
 }
